6d6245a55de8754b266cc223b8c75571a81d18b0
Matthew G Popp <mgp214@gmail.com>
2017-02-05 20:28:40 -0700

    0.1.1
    began work on implementing player system and authoritative server system

---
+++ Assets/_scripts/PlayerBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : Bolt.EntityBehaviour<IPlayerState> {
+
+    public override void Attached() {
+        state.SetTransforms(state.PlayerTransform, transform);
+    }
+}
---
+++ Assets/_scripts/ServerNetworkCallbacks.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[BoltGlobalBehaviour(BoltNetworkModes.Host)]
+public class NetworkCallbacks : Bolt.GlobalEventListener {
+
+    public Dictionary<BoltEntity, BoltConnection> playerList;
+
+    public override void BoltStartDone() {
+        playerList = new Dictionary<BoltEntity, BoltConnection>();
+    }
+
+    public override void Connected(BoltConnection connection) {
+        Debug.Log("New connection from " + connection.RemoteEndPoint.ToString());
+        InstantiatePlayer(connection);
+    }
+    public override void SceneLoadLocalDone(string map) {
+        //instantiate the host player
+        InstantiatePlayer(null);
+    }
+    public void InstantiatePlayer(BoltConnection connection) {
+        Vector3 pos = new Vector3(Random.Range(-16, 16), 0, Random.Range(-16, 16));
+        BoltEntity entity = BoltNetwork.Instantiate(BoltPrefabs.Player, pos, Quaternion.identity);
+        playerList.Add(entity, connection);
+    }
+}
