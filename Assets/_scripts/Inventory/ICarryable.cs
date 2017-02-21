cc3bd6df88906e6050fd6ac87bf7ceea4726ea6f
Matthew G Popp <mgp214@gmail.com>
2017-02-20 18:25:49 -0700

    0.2.1
    Began work on Inventory system.

---
+++ Assets/_scripts/Inventory/ICarryable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public interface ICarryable {
+    float bulk { get; set; }
+    float mass { get; set; }
+    Texture2D thumbnail { get; set; }
+    List<string> displayProperties { get; }
+}
---
+++ Assets/_scripts/Inventory/IEquippable.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IEquippable : ICarryable {
+}
