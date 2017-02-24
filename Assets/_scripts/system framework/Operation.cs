0e2affcb7f6a44e46fe56c15f806b850bafa3353
Matthew G Popp <mgp214@gmail.com>
2017-02-24 13:19:34 -0700

    0.2.4 - further work towards component-level system architecture
    -work on implementing Operations (#7)

--- Assets/_scripts/system framework/Operation.cs
+++ Assets/_scripts/system framework/Operation.cs
@@ -1,5 +1,27 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class Operation {
+public abstract class Operation {
+    public string name { get; protected set; }
+    public Operation(string name) {
+        this.name = name;
+    }
+    public abstract void Update();
+    public abstract void Activate();
+    public abstract bool CanActivate();
+}
+
+public abstract class DiscreteOperation : Operation {
+    public float cooldownTime { get; private set; }
+    private float cooldownRemaining;
+    private List<Effect> _effects;
+    public List<Effect> effects { get { return new List<Effect>(_effects); } }
+    public DiscreteOperation(string name, float cooldownTime, List<Effect> effects = null) : base(name) {
+        this.cooldownTime = cooldownTime;
+        cooldownRemaining = 0f;
+        this._effects = effects;
+    }
+    public void AddEffect(Effect effect) {
+        _effects.Add(effect);
+    }
 }
