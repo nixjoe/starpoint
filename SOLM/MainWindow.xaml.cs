551eee5dbcafb0a9d5f1eded35e2349b2bb496e3
Matthew Popp <mgp214@gmail.com>
2017-03-02 06:49:05 -0700

    0.2.20 SOLM events declaration
    -registered a lot of events that are going to be needed.

--- SOLM/MainWindow.xaml.cs
+++ SOLM/MainWindow.xaml.cs
@@ -25,5 +25,69 @@ namespace SOLM {
         private void lv_operationsList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
 
         }
+
+        private void tb_objectName_TextChanged(object sender, TextChangedEventArgs e) {
+
+        }
+
+        private void tb_objectDryWeight_TextChanged(object sender, TextChangedEventArgs e) {
+
+        }
+
+        private void mi_newLibrary_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void mi_saveLibrary_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void mi_loadLibrary_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void tb_bundleName_TextChanged(object sender, TextChangedEventArgs e) {
+
+        }
+
+        private void tb_libraryName_TextChanged(object sender, TextChangedEventArgs e) {
+
+        }
+
+        private void mi_libraryView_new_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void mi_libraryView_copy_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void mi_libraryView_delete_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void mi_libraryView_sort_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void lv_objectList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void mi_operations_new_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void mi_operations_copy_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void mi_operations_delete_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void mi_operations_sort_Click(object sender, RoutedEventArgs e) {
+
+        }
     }
 }
